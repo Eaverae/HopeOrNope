1e86a0458204589c21ec803b99c3c19616fe1a3c
Guido Schippers <guido_schippers@hotmail.com>
2020-01-23 16:25:58 +0100

    Created the initial app

--- HopeNope/HopeNope/MainPage.xaml.cs
+++ HopeNope/HopeNope/MainPage.xaml.cs
@@ -17,5 +17,31 @@ namespace HopeNope
 		{
 			InitializeComponent();
 		}
+
+		private void ButtonCalculate_Clicked(object sender, EventArgs e)
+		{
+			int firstAge = Convert.ToInt32(EntryFirstAge.Text);
+			int secondAge = Convert.ToInt32(EntrySecondAge.Text);
+
+			int calcA = firstAge > secondAge ? firstAge : secondAge;
+			int calcB = firstAge > secondAge ? secondAge : firstAge;
+
+			int minimum = (calcA / 2) + 7;
+
+			string result = string.Empty;
+
+			if (minimum <= calcB)
+				result = $"Yes you can! {minimum} is the minimum age for your date.";
+			else
+				result = $"Oh hell no! {minimum} is the minimum age for your date!";
+
+			LabelResult.Text = result;
+		}
+
+		private void ButtonReset_Clicked(object sender, EventArgs e)
+		{
+			LabelResult.Text = string.Empty;
+			EntrySecondAge.Text = string.Empty;
+		}
 	}
 }
