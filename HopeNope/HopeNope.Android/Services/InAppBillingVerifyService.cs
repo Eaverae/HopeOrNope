ff2f8560efc7aefeed02e67c7e612b020cc66676
Guido Schippers <guido_schippers@hotmail.com>
2020-03-17 13:37:43 +0100

    comments

--- HopeNope/HopeNope.Android/Services/InAppBillingVerifyService.cs
+++ HopeNope/HopeNope.Android/Services/InAppBillingVerifyService.cs
@@ -6,13 +6,24 @@ using Xamarin.Forms;
 [assembly: Dependency(typeof(InAppBillingVerifyService))]
 namespace HopeNope.Droid.Services
 {
+    /// <summary>
+    /// InAppBillingVerifyService
+    /// </summary>
+    /// <seealso cref="Plugin.InAppBilling.Abstractions.IInAppBillingVerifyPurchase" />
     public class InAppBillingVerifyService : IInAppBillingVerifyPurchase
     {
         const string key1 = @"XOR_key1";
         const string key2 = @"XOR_key2";
         const string key3 = @"XOR_key3";
 
-
+        /// <summary>
+        /// Verifies the purchase.
+        /// </summary>
+        /// <param name="signedData">The signed data.</param>
+        /// <param name="signature">The signature.</param>
+        /// <param name="productId">The product identifier.</param>
+        /// <param name="transactionId">The transaction identifier.</param>
+        /// <returns></returns>
         public Task<bool> VerifyPurchase(string signedData, string signature, string productId = null, string transactionId = null)
         {
             var key1Transform = Plugin.InAppBilling.InAppBillingImplementation.InAppBillingSecurity.TransformString(key1, 1);
--- HopeNope/HopeNope.iOS/Services/InAppBillingVerifyService.cs
+++ HopeNope/HopeNope.iOS/Services/InAppBillingVerifyService.cs
@@ -6,12 +6,20 @@ using Xamarin.Forms;
 [assembly: Dependency(typeof(InAppBillingVerifyService))]
 namespace HopeNope.iOS.Services
 {
+    /// <summary>
+    /// InAppBillingVerifyService
+    /// </summary>
+    /// <seealso cref="Plugin.InAppBilling.Abstractions.IInAppBillingVerifyPurchase" />
     public class InAppBillingVerifyService : IInAppBillingVerifyPurchase
     {
-        const string key1 = @"XOR_key1";
-        const string key2 = @"XOR_key2";
-        const string key3 = @"XOR_key3";
-
+        /// <summary>
+        /// Verifies the purchase.
+        /// </summary>
+        /// <param name="signedData">The signed data.</param>
+        /// <param name="signature">The signature.</param>
+        /// <param name="productId">The product identifier.</param>
+        /// <param name="transactionId">The transaction identifier.</param>
+        /// <returns></returns>
         public Task<bool> VerifyPurchase(string signedData, string signature, string productId = null, string transactionId = null)
         {
             return Task.FromResult(true);
